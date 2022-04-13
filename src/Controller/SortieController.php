1232355cf5e48a8de8bcd84d3091b669f5849194
Raphael Mezui <raphael.mezui@icloud.com>
2022-04-13 17:31:33 +0200

    Ajout des fonctionnalités inscription/désistement + gestion de celles-ci pour l'apparition des boutons

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -151,4 +151,11 @@ class SortieController extends AbstractController
         $sortieRepository->ajoutInscrit($sortie,$user);
         return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
     }
+    #[Route('sortie/activite/desister/{id}', name: 'app_sortie_desister', methods: ['POST','GET'])]
+    public function desister(Sortie $sortie, ParticipantRepository $participantRepository, SortieRepository $sortieRepository):Response{
+        $userVide = $this->getUser()->getUserIdentifier();
+        $user = $participantRepository->findOneBy(['email' => $userVide]);
+        $sortieRepository->removeInscrit($sortie,$user);
+        return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
