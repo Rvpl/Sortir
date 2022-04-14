05006d2ffe37ee11a39e026d928625e6e14b1d50
Raphael Mezui <raphael.mezui@icloud.com>
2022-04-14 12:05:16 +0200

    Remember me + correction connexion

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -45,8 +45,6 @@ class SortieController extends AbstractController
                 'formRecherche' => $form,
                 'sorties' => $sorties,
                 'participants' => $participantRepository->findAll(),
-
-
             ]);
         }else{
 
@@ -148,7 +146,7 @@ class SortieController extends AbstractController
     public function inscription(Sortie $sortie, ParticipantRepository $participantRepository,EtatRepository $etatRepository, SortieRepository $sortieRepository):Response{
         $userVide = $this->getUser()->getUserIdentifier();
         $etat = $etatRepository->findOneBy(['id' => 6]);
-        $user = $participantRepository->findOneBy(['email' => $userVide]);
+        $user = $participantRepository->findOneBy(['pseudo' => $userVide]);
         $sortieRepository->ajoutInscrit($sortie,$user,$etat);
         return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
     }
@@ -156,7 +154,7 @@ class SortieController extends AbstractController
     public function desister(Sortie $sortie, ParticipantRepository $participantRepository,EtatRepository $etatRepository, SortieRepository $sortieRepository):Response{
         $userVide = $this->getUser()->getUserIdentifier();
         $etat = $etatRepository->findOneBy(['id' => 2]);
-        $user = $participantRepository->findOneBy(['email' => $userVide]);
+        $user = $participantRepository->findOneBy(['pseudo' => $userVide]);
         $sortieRepository->removeInscrit($sortie,$user,$etat);
         return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
     }
